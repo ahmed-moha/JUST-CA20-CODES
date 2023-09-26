76b683e7b5fc00fa6fc7b441d88411a1ee4f833f
ahmed-moha <ahmeddhaqan11@gmail.com>
2023-09-26 08:56:42 +0300

    Lesson Two

--- lib/main.dart
+++ lib/main.dart
@@ -13,14 +13,37 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-       
-        body: Center(
-          child: Text("Hellow ",style: TextStyle(
-            fontSize: 30
-          ),),
-
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Icon(Icons.menu),
+        elevation: 0,
+        title: const Text("Welcome"),
+        backgroundColor: Colors.pink,
+        actions: const [
+          Icon(Icons.person),
+          SizedBox(
+            width: 8,
+          ),
+          Icon(Icons.mood)
+        ],
+      ),
+      body: const Center(
+        // Si aad usoo galiso sawir adiga oo ka keensanaayo asset-kaaga 👇
+        child: Image(
+          image: AssetImage("assets/2.jpg"),
         ),
-      );
+
+        // Si aad usoo galiso sawir adiga oo ka keensanaayo internet-ka 👇
+
+        // child: Image(
+        //   image: NetworkImage("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQndKHvfs7Jp3RVVHd4V-2cUl6nHKfAxRQiLvf1ZUhD_Z5ycB0sKg4vWcobHaz76pvSuPc&usqp=CAU"),
+        // ),
+
+        // child: Text(
+        //   "Hello, World ",
+        //   style: TextStyle(fontSize: 30),
+        // ),
+      ),
+    );
   }
 }
