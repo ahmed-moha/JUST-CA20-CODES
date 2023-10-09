446aba3a1f2279cb47f48365999e1234cb7ca868
ahmed-moha <ahmeddhaqan11@gmail.com>
2023-10-09 19:34:06 +0300

    LESSON 3

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
+
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
     const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Home(),
     ),
   );
@@ -28,24 +30,80 @@ class Home extends StatelessWidget {
           Icon(Icons.sports_football)
         ],
       ),
-      body: const Center(
-          child: Image(
-        image: AssetImage("assets/2.jpeg"),
-      )
-          //   image: NetworkImage(
-          //     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_L6zqvKijy3UpsxdVdQO8ygcpUy4yzIh-hQ&usqp=CAU",
-          //   ),
-          // ),
-          // child: Text(
-          //   "Hellow World ",
-          //   style: TextStyle(
-          //     fontSize: 30,
-          //     fontWeight: FontWeight.normal,
-          //     color: Colors.cyan,
-          //   ),
-          //   textAlign: TextAlign.justify,
-          // ),
-          ),
+
+      body: SizedBox(
+        // width: 300,
+        // color: Colors.cyan,
+        child: Column(
+          // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          // crossAxisAlignment:CrossAxisAlignment.stretch ,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                print("HELLO WORLD");
+              },
+              child: const Text("Click me"),
+            ),
+            IconButton(onPressed: () {}, icon: const Icon(Icons.add)),
+            TextButton(onPressed: () {}, child: const Text("Text Button")),
+            ElevatedButton.icon(
+              onPressed: () {},
+              icon: const Icon(Icons.add),
+              label: const Text("Icon Button"),
+            ),
+            OutlinedButton(
+                onPressed: () {}, child: const Text("Outline button"))
+            // Container(
+            //   width: 100,
+            //   height: 100,
+            //   color: Colors.red,
+            // ),
+            // Container(
+            //   width: 100,
+            //   height: 100,
+            //   color: Colors.blue,
+            // ),
+            // Container(
+            //   width: 100,
+            //   height: 100,
+            //   color: Colors.green,
+            // )
+          ],
+        ),
+      ),
+      // body: Center(
+      //   child: Container(
+      //     width: 300,
+      //     height: 300,
+      //     alignment: Alignment.topRight,
+      //     padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 30),
+      //     // padding: const EdgeInsets.only(
+      //     //   top: 20,
+      //     //   left: 20
+      //     // ),
+      //     decoration: BoxDecoration(
+      //         color: Colors.white,
+      //         border: Border.all(
+      //           width: 3.0,
+      //           color: Colors.red,
+      //         ),
+      //         // borderRadius: BorderRadius.circular(50),
+      //         borderRadius: const BorderRadius.only(
+      //           topRight: Radius.circular(50),
+      //           bottomLeft: Radius.circular(50),
+      //         ),
+      //         boxShadow: [
+      //           BoxShadow(
+      //               blurRadius: 25,
+      //               color: Colors.black.withOpacity(0.85),
+      //               offset: const Offset(1, 10))
+      //         ]),
+      //     child: const Text(
+      //       "Container",
+      //       style: TextStyle(fontSize: 30),
+      //     ),
+      //   ),
+      // ),
     );
   }
 }
